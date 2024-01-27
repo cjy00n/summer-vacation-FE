e9a5071d6c95ee09337ebba4072c13bde76abb13
최정윤 <ej921004@naver.com>
2024-01-28 03:44:51 +0900

    Chore : import TopAppBar at HomePage

--- src/pages/HomePage/HomePage.tsx
+++ src/pages/HomePage/HomePage.tsx
@@ -1,5 +1,15 @@
+import { Banner } from "../../components/Home";
+import { TopAppBar } from "../../components/common";
+
 const HomePage = () => {
-  return <div>Home</div>;
+  return (
+    <div>
+      <TopAppBar />
+      <Banner />
+      <h2>내 일기</h2>
+      <p>최근 7일 간 받은 공감 순</p>
+    </div>
+  );
 };
 
 export default HomePage;
