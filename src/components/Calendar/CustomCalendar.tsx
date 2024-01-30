a56f1ecba6c8d71c67e786acb3345127a2a4de7c
최정윤 <ej921004@naver.com>
2024-01-31 00:01:31 +0900

    Feat : add Calendar library

---
+++ src/components/Calendar/CustomCalendar.tsx
@@ -0,0 +1,73 @@
+import { useState } from "react";
+import Calendar from "react-calendar";
+import "react-calendar/dist/Calendar.css";
+import "./CustomCalendar.css";
+import { format } from "date-fns";
+import { ko } from "date-fns/locale";
+
+type ValuePiece = Date | null;
+
+type Value = ValuePiece | [ValuePiece, ValuePiece];
+
+const CustomCalendar = () => {
+  const [selectedDay, setSelectedDay] = useState<Value>(new Date());
+
+  const onChangeDay = (newDate: Value) => {
+    setSelectedDay(newDate);
+  };
+
+  const temp = [
+    { date: new Date("2024-01-09"), text: "😍" },
+    { date: new Date("2024-01-10"), text: "😍" },
+    { date: new Date("2024-01-11"), text: "😍" },
+    { date: new Date("2024-01-27"), text: "😍" },
+    { date: new Date("2024-01-28"), text: "😍" },
+    { date: new Date("2024-01-29"), text: "😄" },
+    { date: new Date("2024-01-30"), text: "😍" },
+  ];
+
+  function tileContent({ date, view }: { date: Date; view: string }) {
+    if (view === "month") {
+      const event = temp.find(
+        (e) =>
+          format(date, "yyyMMdd", { locale: ko }) ===
+          format(e.date, "yyyMMdd", { locale: ko }),
+      );
+      if (event) {
+        return <span>{event.text}</span>;
+      }
+    }
+    return null;
+  }
+
+  function tileClassName({ date, view }: { date: Date; view: string }) {
+    if (view === "month") {
+      const event = temp.find(
+        (e) =>
+          format(date, "yyyMMdd", { locale: ko }) ===
+          format(e.date, "yyyMMdd", { locale: ko }),
+      );
+      if (event) {
+        return "tile-bg-orange";
+      }
+    }
+    return null;
+  }
+
+  return (
+    <div>
+      <Calendar
+        value={selectedDay}
+        onChange={onChangeDay}
+        onClickMonth={() => console.log("달")}
+        formatDay={(_, date) => format(date, "dd")}
+        tileContent={tileContent}
+        tileClassName={tileClassName}
+        next2Label={null}
+        prev2Label={null}
+      />
+    </div>
+  );
+};
+
+export default CustomCalendar;
--- src/pages/CalendarPage/CalendarPage.tsx
+++ src/pages/CalendarPage/CalendarPage.tsx
@@ -1,5 +1,19 @@
+import { CustomCalendar } from "../../components/Calendar";
+
 const CalendarPage = () => {
-  return <>캘린더</>;
+  return (
+    <div>
+      <CustomCalendar />
+      <div className="flex justify-center mt-12">
+        <button className="flex w-28 h-12 p-4 mr-5 items-center justify-center bg-black text-white rounded-[81px]">
+          일기 쓰기
+        </button>
+        <button className="flex w-52 h-12 p-4 items-center justify-center bg-primary-orange text-white rounded-[81px]">
+          일기 보기
+        </button>
+      </div>
+    </div>
+  );
 };
 
 export default CalendarPage;
