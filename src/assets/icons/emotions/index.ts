60b8ab8b1499aa4ab6112d46c4aa980d36708e10
최정윤 <ej921004@naver.com>
2024-01-31 23:28:43 +0900

    Feat : add new Icons

---
+++ src/assets/icons/emotions/index.ts
@@ -0,0 +1,5 @@
+export { default as HappyIcon } from "./HappyIcon";
+export { default as PleasureIcon } from "./PleasureIcon";
+export { default as SadIcon } from "./SadIcon";
+export { default as SoSoIcon } from "./SoSoIcon";
+export { default as AngryIcon } from "./AngryIcon";
---
+++ src/assets/icons/weather/index.ts
@@ -0,0 +1,5 @@
+export { default as SunnyIcon } from "./SunnyIcon";
+export { default as SnowIcon } from "./SnowIcon";
+export { default as RainyIcon } from "./RainyIcon";
+export { default as ThunderIcon } from "./SnowIcon";
+export { default as CloudyIcon } from "./CloudyIcon";
