b90d315364aae255a903affb89d134e03742154b
최정윤 <ej921004@naver.com>
2024-02-01 19:09:14 +0900

    Design : add Main logo image at AuthPage

--- src/pages/AuthPage.tsx
+++ src/pages/AuthPage.tsx
@@ -1,5 +1,4 @@
 import { Link } from "react-router-dom";
-import { RiKakaoTalkFill } from "react-icons/ri";
 import { postKaKaoSignIn } from "../hooks/postKaKaoSignIn";
 
 const AuthPage = () => {
@@ -11,13 +10,13 @@ const AuthPage = () => {
 
   if (code) postKaKaoSignIn(code!, REDIRECT_URI);
 
-  console.log(code);
+  console.log("code : ", code);
   return (
-    <div className="flex flex-col items-center bg-primary-white h-[100vh]">
-      <span>여름방학</span>
+    <div className="flex flex-col items-center content-center justify-center bg-primary-white min-h-[100vh]">
+      <img src="/image/mainImg.svg" />
+      <img src="/logo.svg" />
       <Link to={KAKAO_AUTH_URI}>
-        <button className="flex items-center justify-center bg-primary-yellow w-40 h-12 rounded-[50px]">
-          <RiKakaoTalkFill />
+        <button className="flex items-center justify-center bg-primary-yellow w-40 h-12 rounded-[50px] mt-10">
           <span className="mx-1 text-base font-semibold">카카오 로그인</span>
         </button>
       </Link>
