f311ce242838abca8aee95ff827d5f4192c3417a
최정윤 <ej921004@naver.com>
2024-02-03 21:11:52 +0900

    Design : modify profile item style

--- src/pages/ProfilePage.tsx
+++ src/pages/ProfilePage.tsx
@@ -1,10 +1,11 @@
 import { ko } from "date-fns/locale";
-import { AddPhotoIcon, EditIcon } from "../assets/icons";
+import { AddPhotoIcon, EditIcon, KebabMenuIcon } from "../assets/icons";
 import { TopAppBar } from "../components/common";
 import { format } from "date-fns";
 import { useNavigate } from "react-router-dom";
 import { ROUTE } from "../routes/Route";
 import { Diary } from "../types";
+import GetEmotionIcon from "../assets/icons/emotions/GetEMotionIcon";
 
 const ProfilePage = () => {
   const navigate = useNavigate();
@@ -26,7 +27,7 @@ const ProfilePage = () => {
       id: "1",
       image: "/sample.webp",
       date: new Date("2024-01-03"),
-      emotion: "기뻐요",
+      emotion: "화나요",
       weather: "맑음",
       title: "오늘의 일기",
       contents: "오늘의 일기입니다",
@@ -38,7 +39,7 @@ const ProfilePage = () => {
       id: "1",
       image: "/sample.webp",
       date: new Date("2024-02-03"),
-      emotion: "기뻐요",
+      emotion: "좋아요",
       weather: "맑음",
       title: "오늘의 일기",
       contents: "오늘의 일기입니다",
@@ -64,7 +65,7 @@ const ProfilePage = () => {
   const classifyByMonth = (items: Diary[]) => {
     const obj: { [key: string]: Diary[] } = {};
     items.forEach((item) => {
-      const key = format(item.date as Date, "yy년 MM월");
+      const key = format(item.date as Date, "yyyy년 MM월");
       if (!obj[key]) obj[key] = [];
       obj[key].push(item);
     });
@@ -76,12 +77,12 @@ const ProfilePage = () => {
 
   /* 피드 상세 페이지로 이동 */
   const linkToDetailPage = (id: string) => {
-    navigate(ROUTE.FEED_DETAIL_PAGE.link + id);
+    navigate(ROUTE.FEED_DETAIL_PAGE.link + `/${id}`);
   };
 
   return (
     <>
-      <TopAppBar title="프로필" />
+      <TopAppBar title="프로필" rightIcon={<KebabMenuIcon />} />
       <div className="flex justify-between px-14 py-8">
         <div className="flex h-20 w-20 items-center justify-center rounded-full bg-gray-80">
           <AddPhotoIcon />
@@ -96,22 +97,34 @@ const ProfilePage = () => {
         </div>
       </div>
       <div>
-        <div className="py-4">
+        <div className="bg-gray-90 py-4">
           {Object.keys(classifiedDiaries).map((key) => (
             <div className="my-2" key={key}>
-              <span>{key}</span>
-              {classifiedDiaries[key].map(({ image, id, date }, idx) => (
-                <div
-                  key={"profileFeedItem" + id + idx}
-                  className="relative grid grid-cols-3 p-[1px]"
-                  onClick={() => linkToDetailPage(id)}
-                >
-                  <img src={image} className="h-[33vw] w-[33vw] object-cover" />
-                  <span className="absolute bottom-1 left-1 text-xs font-semibold text-primary-white">
-                    {format(date as Date, "MM/dd(eee)", { locale: ko })}
-                  </span>
-                </div>
-              ))}
+              <span className="flex items-center p-2 text-base font-semibold">
+                {key}
+              </span>
+              <div className="grid grid-cols-3">
+                {classifiedDiaries[key].map(
+                  ({ image, id, date, emotion }, idx) => (
+                    <div
+                      key={"profileFeedItem" + id + idx}
+                      className="relative h-[33vw] w-[33vw] p-[1px]"
+                      onClick={() => linkToDetailPage(id)}
+                    >
+                      <img
+                        src={image}
+                        className="h-[33vw] w-[33vw] object-cover"
+                      />
+                      <span className="absolute bottom-1 left-1 flex w-full items-center justify-between pl-1 pr-3 text-sm font-semibold text-primary-white">
+                        {format(date as Date, "MM/dd(eee)", { locale: ko })} |
+                        <span className="h-4 w-4 rounded-full bg-primary-yellow">
+                          <GetEmotionIcon emotion={emotion} />
+                        </span>
+                      </span>
+                    </div>
+                  ),
+                )}
+              </div>
             </div>
           ))}
         </div>
