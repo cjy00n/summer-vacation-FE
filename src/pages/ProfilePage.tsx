1d9efea8b19b8e016490e5a6822df2ffc381a21d
최정윤 <ej921004@naver.com>
2024-02-03 22:37:04 +0900

    Design : add weather at ProfilePage

--- src/pages/ProfilePage.tsx
+++ src/pages/ProfilePage.tsx
@@ -6,6 +6,7 @@ import { useNavigate } from "react-router-dom";
 import { ROUTE } from "../routes/Route";
 import { Diary } from "../types";
 import GetEmotionIcon from "../assets/icons/emotions/GetEMotionIcon";
+import GetWeatherIcon from "../assets/icons/weather/GetWeatherIcon";
 
 const ProfilePage = () => {
   const navigate = useNavigate();
@@ -52,7 +53,7 @@ const ProfilePage = () => {
       image: "/sample.webp",
       date: new Date("2023-12-31"),
       emotion: "기뻐요",
-      weather: "맑음",
+      weather: "천둥",
       title: "오늘의 일기",
       contents: "오늘의 일기입니다",
       isWrite: 1,
@@ -97,15 +98,18 @@ const ProfilePage = () => {
         </div>
       </div>
       <div>
-        <div className="bg-gray-90 py-4">
+        <div className="bg-primary-orange px-3 py-3 text-lg font-bold text-primary-white">
+          내 일기
+        </div>
+        <div className="bg-gray-90">
           {Object.keys(classifiedDiaries).map((key) => (
-            <div className="my-2" key={key}>
-              <span className="flex items-center p-2 text-base font-semibold">
+            <div className="" key={key}>
+              <span className="flex items-center p-3 text-base font-semibold">
                 {key}
               </span>
               <div className="grid grid-cols-3">
                 {classifiedDiaries[key].map(
-                  ({ image, id, date, emotion }, idx) => (
+                  ({ image, id, date, emotion, weather }, idx) => (
                     <div
                       key={"profileFeedItem" + id + idx}
                       className="relative h-[33vw] w-[33vw] p-[1px]"
@@ -115,18 +119,24 @@ const ProfilePage = () => {
                         src={image}
                         className="h-[33vw] w-[33vw] object-cover"
                       />
-                      <span className="absolute bottom-1 left-1 flex w-full items-center justify-between pl-1 pr-3 text-sm font-semibold text-primary-white">
-                        {format(date as Date, "MM/dd(eee)", { locale: ko })} |
-                        <span className="h-4 w-4 rounded-full bg-primary-yellow">
-                          <GetEmotionIcon emotion={emotion} />
+                      <div className="absolute bottom-1 left-1 flex w-[25vw] items-center justify-around bg-black bg-opacity-35 px-1">
+                        <span className="text-sm font-semibold text-primary-white">
+                          {format(date as Date, "dd(eee)", { locale: ko })} |
                         </span>
-                      </span>
+                        <span>
+                          <GetWeatherIcon weather={weather} fillColor="white" />
+                        </span>
+                        <span>
+                          <GetEmotionIcon emotion={emotion} fillColor="white" />
+                        </span>
+                      </div>
                     </div>
                   ),
                 )}
               </div>
             </div>
           ))}
+          d
         </div>
       </div>
     </>
