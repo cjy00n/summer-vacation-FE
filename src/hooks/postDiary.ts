a44b67f2fdce48660421f8e0336ae589be9a7bf4
최정윤 <ej921004@naver.com>
2024-02-16 02:04:14 +0900

    Chore : pass formdata at postDiary

--- src/hooks/postDiary.ts
+++ src/hooks/postDiary.ts
@@ -8,9 +8,20 @@ type PostDrawingType = Omit<Diary, "id" | "createdAt">;
 export const postDiary = async (diary: PostDrawingType) => {
   try {
     console.log(diary);
+
+    const formData = new FormData();
+    formData.append("title", diary.title);
+    formData.append("text", diary.contents);
+    formData.append("date", diary.date!.toString());
+    formData.append("input", diary.image);
+    formData.append("emotion", diary.emotion);
+    formData.append("weather", diary.weather);
+    formData.append("isPublic", diary.isPublic.toString());
+    formData.append("isWirte", diary.isWrite.toString());
+
     const response = await instance.post<{ result: string }>(
       "diary/create-diary",
-      { ...diary, text: diary.contents, imageUrl: diary.image },
+      formData,
     );
     console.log(response.data);
     return response.data;
