537095cb912dc8de5648caeeb360156d1d93ada7
최정윤 <ej921004@naver.com>
2024-02-16 19:34:41 +0900

    Feat : add postLike hooks

---
+++ src/hooks/postLike.ts
@@ -0,0 +1,21 @@
+import { instance } from ".";
+import { useMutation, useQueryClient } from "react-query";
+
+export const postLike = async ({ id }: { id: string }) => {
+  try {
+    const response = await instance.post("/like/" + id);
+    return response.data.message;
+  } catch (e) {
+    console.error(e);
+  }
+};
+
+export const usePostLike = (id: string) => {
+  const queryClient = useQueryClient();
+
+  return useMutation(() => postLike({ id }), {
+    onSuccess: () => {
+      queryClient.invalidateQueries(["getDiary"] + id);
+    },
+  });
+};
