ddf255f68f53806ef6348310d1779834e0fc134f
최정윤 <ej921004@naver.com>
2024-02-16 17:55:04 +0900

    Chore : setActiveBottomTab value at CalendarPage

--- src/pages/CalendarPage.tsx
+++ src/pages/CalendarPage.tsx
@@ -1,7 +1,15 @@
+import { useRecoilState } from "recoil";
 import { CustomCalendar } from "../components/Calendar";
 import { TopAppBar } from "../components/common";
+import { bottomTabState } from "../recoil/atoms/bottomTabState";
+import { useEffect } from "react";
 
 const CalendarPage = () => {
+  const [, setActiveBottomTab] = useRecoilState(bottomTabState);
+  useEffect(() => {
+    setActiveBottomTab("CALENDAR");
+  }, [setActiveBottomTab]);
+
   return (
     <div className="bg-primary-white">
       <TopAppBar title="달력" />
