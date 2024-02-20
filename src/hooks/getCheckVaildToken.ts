64372aef5ffc55795120b3b0137179a37e734206
최정윤 <ej921004@naver.com>
2024-02-21 06:20:30 +0900

    Feat : add getCheckValidToken

---
+++ src/hooks/getCheckVaildToken.ts
@@ -0,0 +1,36 @@
+import axios from "axios";
+import { instance } from ".";
+import { useQuery } from "react-query";
+
+export const getCheckVaildToken = async () => {
+  const accessToken = localStorage.getItem("accessToken");
+
+  if (accessToken) {
+    try {
+      const response = await instance.post<boolean | "incorrect format">(
+        "users/access-token?accessToken=" + accessToken,
+      );
+
+      if (response.data !== undefined) {
+        console.log(response.data);
+        return response.data;
+      }
+    } catch (e) {
+      console.error(e);
+
+      if (axios.isAxiosError(e) && e.response && e.response.status === 500) {
+        return "incorrect format";
+      } else if (
+        axios.isAxiosError(e) &&
+        e.response &&
+        e.response.status === 400
+      ) {
+        return false;
+      }
+    }
+  }
+};
+
+export const useGetCheckVaildToken = () => {
+  return useQuery(["check-access-token"], () => getCheckVaildToken());
+};
