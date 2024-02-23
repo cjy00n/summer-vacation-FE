2bb1c70e639c602f224fe5c45759d27db7bfa44f
최정윤 <ej921004@naver.com>
2024-02-23 23:46:56 +0900

    Feat : split postDrawingButton conponent

---
+++ src/components/AddDiary/RequestDrawingButton.tsx
@@ -0,0 +1,62 @@
+import { useRecoilState } from "recoil";
+import { useGetUserInfo } from "../../hooks/getMyUserInfo";
+import { postDiaryDrawing } from "../../hooks/postDiaryDrawing";
+import { Emotion, Weather } from "../../types";
+import { drawingRecordState } from "../../recoil/atoms/drawingRecordState";
+import { useState } from "react";
+import { DrawingModal } from ".";
+import { updateDrawingRecord } from "../../recoil/utils/updateDrawingRecord";
+
+interface RequestDrawingButton {
+  children: React.ReactNode;
+  input: string;
+  weather: Weather;
+  emotion: Emotion;
+  handleFinish?: () => void;
+}
+
+const RequestDrawingButton = ({
+  children,
+  input,
+  emotion,
+  weather,
+  handleFinish,
+}: RequestDrawingButton) => {
+  const [drawingRecord, setDrawingRecord] = useRecoilState(drawingRecordState);
+  const [drawingModalOpen, setDrawingModalOpen] = useState(false);
+
+  const { data: userInfo } = useGetUserInfo();
+
+  const handleDrawing = async () => {
+    setDrawingModalOpen(true);
+    try {
+      const newImage = await postDiaryDrawing({
+        input: input,
+        emotion: emotion,
+        weather: weather,
+        gender: userInfo!.gender,
+      });
+
+      if (newImage) {
+        updateDrawingRecord(setDrawingRecord, {
+          ...drawingRecord,
+          beforeImages: [...drawingRecord.beforeImages, newImage],
+          remainingTries: drawingRecord.remainingTries - 1,
+        });
+        if (handleFinish) handleFinish();
+        setDrawingModalOpen(false);
+      }
+    } catch (e) {
+      console.error(e);
+    }
+  };
+
+  return (
+    <div onClick={handleDrawing}>
+      {children}
+      <DrawingModal open={drawingModalOpen} />
+    </div>
+  );
+};
+
+export default RequestDrawingButton;
