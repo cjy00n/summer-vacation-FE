d706c60f08ec41366157aff324477b18a92163c5
최정윤 <ej921004@naver.com>
2024-02-26 00:29:01 +0900

    Feat : add infinite scroll of FeedPage

--- src/components/Feed/FeedContainer.tsx
+++ src/components/Feed/FeedContainer.tsx
@@ -1,22 +1,62 @@
+import { useEffect } from "react";
 import { FeedItem } from ".";
-import { useGetPublicDiary } from "../../hooks/getPublicDiary";
+import { getPublicDiary } from "../../hooks/getPublicDiary";
+import { useInfiniteQuery } from "react-query";
+import { useInView } from "react-intersection-observer";
+import { Diary } from "../../types";
 
 const FeedContainer = () => {
-  const { data: publicDiaries } = useGetPublicDiary();
+  const [ref, isView] = useInView();
+
+  const fetchPublicData = async ({ pageParam = 1 }) => {
+    const response = await getPublicDiary(pageParam);
+    return response;
+  };
+
+  const {
+    data: diaryData,
+    fetchNextPage,
+    hasNextPage,
+  } = useInfiniteQuery(["getPublicDiary"], fetchPublicData, {
+    getNextPageParam: (lastPage, pages) => {
+      console.log(pages.length, pages, "last", lastPage);
+      if (lastPage && lastPage?.length > 0) {
+        return pages.length + 1;
+      }
+      return undefined;
+    },
+  });
+
+  useEffect(() => {
+    if (isView && hasNextPage) {
+      fetchNextPage();
+    }
+  }, [isView, hasNextPage, fetchNextPage, diaryData]);
+
+  // 데이터 통합 처리
+  const diaryItems =
+    diaryData?.pages
+      ?.flat()
+      .map((item) => item)
+      .filter(
+        (item): item is { diary: Diary; totalCount: number } =>
+          !!item && !!item.diary,
+      ) ?? [];
 
   return (
     <>
       <div className="grid grid-flow-dense grid-cols-3 grid-rows-3">
-        {publicDiaries &&
-          publicDiaries.map(({ diary, totalCount }, idx) => (
+        {diaryItems &&
+          diaryItems.map(({ diary, totalCount }, idx) => (
             <FeedItem
-              key={"feed-item-" + idx}
               image={diary.imageUrl}
               idx={idx}
               like={totalCount}
               _id={diary.id}
+              key={"feedItem" + diary.id}
             />
           ))}
+        <div ref={ref} />
       </div>
     </>
   );
--- src/hooks/getPublicDiary.ts
+++ src/hooks/getPublicDiary.ts
@@ -2,17 +2,19 @@ import { useQuery } from "react-query";
 import { instance } from ".";
 import { Diary } from "../types";
 
-const getPublicDiary = async (page = 1) => {
+export const getPublicDiary = async (page = 1, pageSize = 12) => {
   try {
     const response = await instance.get<{ diary: Diary; totalCount: number }[]>(
-      `diary/get-diaries/1?page=${page}&pageSize=10`,
+      `diary/get-diaries/1?page=${page}&pageSize=${pageSize}`,
     );
-    if (response.data) return response.data;
+    if (response.data) {
+      return response.data;
+    }
   } catch (e) {
     console.error;
   }
 };
 
-export function useGetPublicDiary(page?: number) {
-  return useQuery(["getPublicDiary"], () => getPublicDiary(page));
+export function useGetPublicDiary(page?: number, pageSize?: number) {
+  return useQuery(["getPublicDiary"], () => getPublicDiary(page, pageSize));
 }
