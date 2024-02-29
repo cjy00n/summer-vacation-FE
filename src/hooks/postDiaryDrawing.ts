58d2d6d9919afcd033f776d9b79ec806656e4202
정윤 <jung-yoon@choejeong-yun-ui-MacBookAir.local>
2024-03-01 00:15:49 +0900

    Chore : rid of weather options

--- src/hooks/postDiaryDrawing.ts
+++ src/hooks/postDiaryDrawing.ts
@@ -1,22 +1,20 @@
 import { instance } from ".";
 import { getEnglishEmotion } from "../assets/data/emotionData";
-import { getEnglishWeather } from "../assets/data/weatherData";
 import { Emotion, Weather } from "../types";
 
 interface postDiaryDrawingProps {
   input: string;
   emotion: Emotion;
-  weather: Weather;
+  weather?: Weather;
   gender?: "남자" | "여자";
 }
 
 export const postDiaryDrawing = async ({
   input,
   emotion,
-  weather,
   gender,
 }: postDiaryDrawingProps) => {
-  input = `The weather today is ${getEnglishWeather(weather)}, and My feeling is so ${getEnglishEmotion(emotion)}. ${input}
+  input = ` My feeling is so ${getEnglishEmotion(emotion)}. ${input}
 Like a cute and colorful but simple webtoon drawn with crayon without ugly things and any text `;
   if (gender)
     input += `FYI, I am a ${gender === "남자" ? "man person" : "woman person"}`;
