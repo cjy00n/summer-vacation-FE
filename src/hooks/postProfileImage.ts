f6a3af21f893c80cae8ba594bccca06b59753325
cjy00n <ej921004@naver.com>
2024-03-12 04:59:47 +0900

    Feat : alert error when profile Image size is over 1MB

--- src/hooks/postProfileImage.ts
+++ src/hooks/postProfileImage.ts
@@ -1,6 +1,7 @@
 import { message } from "antd";
 import { instance } from ".";
 import { useMutation } from "react-query";
+import { AxiosError } from "axios";
 
 export const postProfileImage = async (imageFile: File) => {
   try {
@@ -8,13 +9,19 @@ export const postProfileImage = async (imageFile: File) => {
     formData.append("imageFile", imageFile);
 
     const response = await instance.post("users/profile-image", formData);
+
     if (response.data) {
       if (response.status === 201) {
         message.success("프로필 사진이 변경되었습니다.");
+        return response.data;
       }
-      return response.data;
     }
   } catch (e) {
+    const { response } = e as AxiosError;
+
+    if (response?.status === 413) {
+      message.error("프로필 사진은 1MB 이하의 이미지만 가능합니다.");
+    }
     console.error(e);
   }
 };
