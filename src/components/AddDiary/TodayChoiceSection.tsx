9b7568ecc27bd65d0855034c810a962fa5427bf9
cjy00n <ej921004@naver.com>
2024-03-26 16:42:34 +0900

    Design : change to new emotion icons

--- src/components/AddDiary/TodayChoiceSection.tsx
+++ src/components/AddDiary/TodayChoiceSection.tsx
@@ -29,8 +29,8 @@ const TodayChoiceSection = ({
             >
               <EmotionIcon
                 emotion={item as Emotion}
-                width={24}
-                height={24}
+                width={32}
+                height={32}
                 fillColor={`${emotion === item ? "white" : "#BAB6B4"}`}
               />
             </button>
