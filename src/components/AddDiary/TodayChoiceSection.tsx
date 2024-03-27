673b4873cf3db7d6288a9e2ae6775e8fcb59b43e
cjy00n <ej921004@naver.com>
2024-03-27 18:01:03 +0900

    Design : modify emotion icon size

--- src/components/AddDiary/TodayChoiceSection.tsx
+++ src/components/AddDiary/TodayChoiceSection.tsx
@@ -29,8 +29,8 @@ const TodayChoiceSection = ({
             >
               <EmotionIcon
                 emotion={item as Emotion}
-                width={32}
-                height={32}
+                width={24}
+                height={24}
                 fillColor={`${emotion === item ? "white" : "#BAB6B4"}`}
               />
             </button>
