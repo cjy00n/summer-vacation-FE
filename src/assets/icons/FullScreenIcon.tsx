0c33cef37595282e062ac71131815ddab0488677
cjy00n <ej921004@naver.com>
2024-04-21 04:45:05 +0900

    Chore : modify clip-path tag

--- src/assets/icons/FullScreenIcon.tsx
+++ src/assets/icons/FullScreenIcon.tsx
@@ -6,7 +6,7 @@ const FullScreenIcon = () => (
     fill="none"
     xmlns="http://www.w3.org/2000/svg"
   >
-    <g clip-path="url(#clip0_763_64350)">
+    <g clipPath="url(#clip0_763_64350)">
       <path
         fillRule="evenodd"
         clipRule="evenodd"
